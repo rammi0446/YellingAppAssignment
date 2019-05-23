346671739be0953f89f37a0446ddc489f44ed6ec
rammi0446 <rammi.0446@gmail.com>
2019-05-23 14:31:58 -0400

    R3-Green

--- src/Yelling.java
+++ src/Yelling.java
@@ -1,10 +1,34 @@
 
 public class Yelling{
+	
+	//check upper case additional function
+private static boolean isStringUpperCase(String str){
+        
+        //convert String to char array
+        char[] charArray = str.toCharArray();
+        
+        for(int i=0; i < charArray.length; i++){
+            
+            //if any character is not in upper case, return false
+            if( !Character.isUpperCase( charArray[i] ))
+                return false;
+        }
+        
+        return true;
+    }
+//////////////////////
 public String Scream(String name)
 {
 	if(name != "null")
 	{
-	return name + " is yelling";
+		String yelling = " is yelling";
+		boolean checkCase = isStringUpperCase(name);
+		if(checkCase == true)
+		{
+			yelling = yelling.toUpperCase();
+		}
+		
+	return name + yelling;
 	}
 	return "Nobody is yelling";
 	
